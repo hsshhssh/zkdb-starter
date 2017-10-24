be98f8c9611e99c9fb7cef3c84e77a5796d08544
hsshhsshhssh <346731187@qq.com>
2017-10-24 20:29:16 +0800

    单独初始化数据源 配置文件分离

--- src/main/java/org/hssh/common/ZkdbcpConfig.java
+++ src/main/java/org/hssh/common/ZkdbcpConfig.java
@@ -1,17 +1,21 @@
 package org.hssh.common;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.github.zkclient.ZkClient;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by hssh on 2017/2/17.
@@ -49,7 +53,7 @@ public class ZkdbcpConfig {
     //}
 
     @Bean(name = "dataSource")
-    public AbstractRoutingDataSource zKDynamicDataSource() throws SQLException {
+    public DataSource zKDynamicDataSource() throws SQLException {
         ZkDynamicDataSource zkDynamicDataSource = new ZkDynamicDataSource();
 
         if(null == zkClient) {
@@ -60,6 +64,80 @@ public class ZkdbcpConfig {
         }
 
         return zkDynamicDataSource;
+    }
+
+    /**
+     * 获取主从数据源
+     */
+    public DataSource createDataSource(String dataSourceName) throws SQLException
+    {
+        ZkClient zk;
+        if(null == zkClient)
+        {
+            zk = new ZkClient(System.getenv("ZK_HOST"));
+        }
+        else
+        {
+            zk = zkClient;
+        }
+
+        PropertiesConfiguration dbConfig = ZkUtils.byteToProper(zk.readData("/config/zkdb/" + getConfName(this.properties)));
+        PropertiesConfiguration bizConfig = ZkUtils.byteToProper(zk.readData("/config/zkdb/" + properties.getBizName()));
+
+        Preconditions.checkNotNull(dbConfig, "load zk dbConfig error");
+        DruidDataSource dataSource = new DruidDataSource();
+        List<String> bizConfigList = Splitter.on(" ").splitToList(bizConfig.getString(dataSourceName));
+        Preconditions.checkArgument(bizConfigList != null && bizConfigList.size()>=2, "zkdb bizConfig error");
+
+        String url = String.format(dbConfig.getString(dataSourceName + ".url"), bizConfigList.get(0));
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(bizConfigList.get(1));
+        dataSource.setPassword(bizConfigList.size()==2 ? "" : bizConfigList.get(2));
+        dataSource.init();
+
+        return dataSource;
+    }
+
+    /**
+     * 获取备份数据数据源
+     */
+    public DataSource createBkDataSource() throws SQLException
+    {
+        final String backupDataSourceName = "backup";
+
+        ZkClient zk;
+        if(null == zkClient)
+        {
+            zk = new ZkClient(System.getenv("ZK_HOST"));
+        }
+        else
+        {
+            zk = zkClient;
+        }
+
+        PropertiesConfiguration backupDbConfig = ZkUtils.byteToProper(zk.readData("/config/zkdb/" + getConfName(this.properties)));
+        PropertiesConfiguration bizConfig = ZkUtils.byteToProper(zk.readData("/config/zkdb/" + properties.getBizName()));
+
+        Preconditions.checkNotNull(backupDbConfig, "load zk dbConfig error");
+        DruidDataSource dataSource = new DruidDataSource();
+        List<String> bizConfigList = Splitter.on(" ").splitToList(bizConfig.getString(backupDataSourceName));
+        Preconditions.checkArgument(bizConfigList != null && bizConfigList.size()>=2, "zkdb bizConfig error");
+
+        String url = backupDbConfig.getString(backupDataSourceName + ".url");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(bizConfigList.get(1));
+        dataSource.setPassword(bizConfigList.size()==2 ? "" : bizConfigList.get(2));
+        dataSource.init();
+
+        return dataSource;
+    }
+
+    public static String getConfName(ZkdbcpConfigProperties properties)
+    {
+        return properties.getConfName() == null ? "db.conf" : properties.getConfName();
+
     }
 
 }
